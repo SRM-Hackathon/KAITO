e932c8f2626529da0e70828e35942d050ccc9efe
yogya-ch <33741788+yogya-ch@users.noreply.github.com>
2019-10-12 12:04:53 +0530

    Add files via upload

---
+++ auto.js
@@ -0,0 +1,26 @@
+var map = L.map('map');
+
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+var control = L.Routing.control(L.extend(window.lrmConfig, {
+	waypoints: [
+		L.latLng(12.934631, 77.533980),
+		L.latLng(12.887702, 77.642015)
+	],
+	serviceUrl: 'http://127.0.0.1:5000/route/v1',
+	geocoder: L.Control.Geocoder.nominatim(),
+	routeWhileDragging: true,
+	reverseWaypoints: true,
+	showAlternatives: true,
+	altLineOptions: {
+		styles: [
+			{color: 'black', opacity: 0.15, weight: 9},
+			{color: 'white', opacity: 0.8, weight: 6},
+			{color: 'blue', opacity: 0.5, weight: 2}
+		]
+	}
+})).addTo(map);
+
+L.Routing.errorControl(control).addTo(map);
